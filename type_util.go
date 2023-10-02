4a89c818a8d6adf05937fe1526c47aa30d8d7dc2
kmou424 <me@kmou424.moe>
2023-10-03 03:42:03 +0800

    :sparkles: feat: add ActualTypeOf tool

--- type_util.go
+++ type_util.go
@@ -5,3 +5,11 @@ import "reflect"
 func TypeOf[T any]() reflect.Type {
 	return reflect.TypeOf((*T)(nil))
 }
+
+func ActualTypeOf[T any]() reflect.Type {
+	vType := TypeOf[T]()
+	if vType.Kind() == reflect.Ptr {
+		vType = vType.Elem()
+	}
+	return vType
+}
