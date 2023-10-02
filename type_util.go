e5e81e7b4dbafa750fe89c4447fd7f22b233e119
kmou424 <me@kmou424.moe>
2023-10-03 03:54:21 +0800

    :zap: perf(type): better implementation of TypeOf and ActualTypeOf

--- type_util.go
+++ type_util.go
@@ -2,13 +2,18 @@ package ohanakoutilgo
 
 import "reflect"
 
+// TypeOf get the type of the generic type
 func TypeOf[T any]() reflect.Type {
-	return reflect.TypeOf((*T)(nil))
+	return reflect.TypeOf((*T)(nil)).Elem()
 }
 
+// ActualTypeOf recursively get the non-pointer type of the generic type
 func ActualTypeOf[T any]() reflect.Type {
 	vType := TypeOf[T]()
-	if vType.Kind() == reflect.Ptr {
+	for {
+		if vType.Kind() != reflect.Ptr {
+			break
+		}
 		vType = vType.Elem()
 	}
 	return vType
